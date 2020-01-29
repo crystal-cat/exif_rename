d68c7c02e8ac51af6ffedea12bf11e09efa1e582
Krista Karppinen <krista.celestia@gmail.com>
2020-01-29 19:36:13 +0200

    Add option to use different date sources
    Add a command line option (-d) to specify date source (currently
    exif, date-created and date-modified). You can specify more than
    one date source, in case one of them fails (like file doesn't
    contain any exif data)

--- exif_rename.py
+++ exif_rename.py
@@ -5,7 +5,7 @@ A tool for batch renaming image files based on their (creation) date.
 """
 
 __author__ = "Krista Karppinen"
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 __copyright__ = "Copyright (C) 2020 Krista Karppinen"
 __license__  = """License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
 This is free software: you are free to change and redistribute it.
@@ -23,7 +23,10 @@ import subprocess
 import sys
 
 
-class ExifReadException(Exception):
+class TimestampReadException(Exception):
+    pass
+
+class CommandLineParseException(Exception):
     pass
 
 simulated_filelist = []
@@ -51,13 +54,34 @@ exif_date_pattern = re.compile('^(\\d{4}):(\\d{2}):(\\d{2}) (\\d{2}):(\\d{2}):(\
 def get_exif_timestamp(filename):
     exif_dict = piexif.load(filename)
     if len(exif_dict["Exif"]) == 0:
-        raise ExifReadException("File {0} does not contain any EXIF data!".format(filename))
+        raise TimestampReadException("File {0} does not contain any EXIF data!".format(filename))
     if not piexif.ExifIFD.DateTimeDigitized in exif_dict["Exif"]:
-        raise ExifReadException("File {0} does not contain an EXIF timestamp.".format(filename))
+        raise TimestampReadException("File {0} does not contain an EXIF timestamp.".format(filename))
 
     datetime_str = exif_dict["Exif"][piexif.ExifIFD.DateTimeDigitized].decode()
     datetime_tuple = list(map(int, exif_date_pattern.match(datetime_str).groups()))
     return datetime.datetime(datetime_tuple[0], datetime_tuple[1], datetime_tuple[2], datetime_tuple[3], datetime_tuple[4], datetime_tuple[5])
+
+def get_stat_timestamp(filename, timestamp_type):
+    statinfo = os.stat(filename)
+    return datetime.datetime.fromtimestamp(getattr(statinfo, timestamp_type))
+
+def get_timestamp(filename, date_sources):
+    for date_source in date_sources:
+        if date_source == 'exif':
+            try:
+                return get_exif_timestamp(filename)
+            except TimestampReadException as e:
+                print(e, file=sys.stderr)
+
+        elif date_source == 'file-created':
+            return get_stat_timestamp(filename, 'st_ctime')
+        elif date_source == 'file-modified':
+            return get_stat_timestamp(filename, 'st_mtime')
+        else:
+            raise CommandLineParseException('Unknown date source: ' + date_source)
+
+    raise TimestampReadException('No more date sources to try!')
 
 def main(args):
     ext = "jpg"
@@ -69,7 +93,7 @@ def main(args):
             continue
 
         try:
-            dt = get_exif_timestamp(filename)
+            dt = get_timestamp(filename, args.date_sources)
             formatted_date = dt.strftime(args.date_format)
             if matches_timestamp(filename, formatted_date, ext):
                 print("File {0} unmodified (file name already matches exif data)".format(filename), file=sys.stderr)
@@ -83,21 +107,38 @@ def main(args):
             else:
                 subprocess.run(cmd_list + [filename, to_filename])
 
-        except ExifReadException as e:
+        except TimestampReadException as e:
             print(e, file=sys.stderr)
 
+def parse_move_command(args):
+    if args.git_mv:
+        if args.mv_cmd_raw != "mv":
+            raise CommandLineParseException("Conflicting options specified: -g and -m")
+        return "git mv"
+
+    return args.mv_cmd_raw
+
+def parse_date_sources(args):
+    sources = args.date_source_str.split(',')
+    for source in sources:
+        if source not in ('exif', 'file-created', 'file-modified'):
+            raise CommandLineParseException('Unknown date source: ' + source)
+
+    return sources
 
 if __name__ == "__main__":
-    default_dateformat = "%%Y-%%m-%%d_%%H.%%M.%%S"
+    default_dateformat = "%Y-%m-%d_%H.%M.%S"
+    default_dateformat_help = default_dateformat.replace('%', '%%')
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
     
     # Files to process
     parser.add_argument("files", nargs="+", metavar="FILE", help="List of files to process")
 
     # Options
-    parser.add_argument("-f", "--date-format", action="store", metavar="fmt", default=default_dateformat, help="Specify a custom date format (default " + default_dateformat + ", see man (1) date for details)")
+    parser.add_argument("-d", "--date-source", action="store", dest="date_source_str", metavar="src", default="exif", help="Specify the date source(s) to try in order, comma-separated (exif, file-created, file-modified)")
+    parser.add_argument("-f", "--date-format", action="store", metavar="fmt", default=default_dateformat, help="Specify a custom date format (default " + default_dateformat_help + ", see man (1) date for details)")
     parser.add_argument("-g", "--git-mv", action="store_true", default=False, help="Use git mv instead of regular mv for renaming")
-    parser.add_argument("-m", "--mv-cmd", action="store", metavar="cmd", default="mv", dest="mv_cmd", help="Specify a command to use for renaming instead of mv")
+    parser.add_argument("-m", "--mv-cmd", action="store", metavar="cmd", default="mv", dest="mv_cmd_raw", help="Specify a command to use for renaming instead of mv")
     parser.add_argument("-s", "--simulate", action="store_true", default=False, help="Simulate only (print what would be done, don't do anything)")
 
     # Specify output of "--version"
@@ -109,12 +150,14 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    # Resolve conflicts
-    if args.git_mv:
-        if args.mv_cmd != "mv":
-            print("Conflicting options specified: -g and -m")
-            sys.exit(1)
-        args.mv_cmd = "git mv"
+    try:
+        # Do additional parsing
+        args.mv_cmd = parse_move_command(args)
+        args.date_sources = parse_date_sources(args)
+
+    except CommandLineParseException as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
 
     main(args)
 
