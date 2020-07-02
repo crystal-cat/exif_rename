5a8499e539ccce97106dd22f3c6f868561a916e1
Fiona Klute <fiona.klute@gmx.de>
2020-07-02 08:55:53 +0200

    Catch TimestampReadExceptions from all timestamp sources
    This makes fallthrough to the first working source work. The commit
    also adds tests for that, reaching 100% coverage of the
    exif_rename.get_timestamp() function.

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ from pathlib import Path
 datadir = Path(__file__).parent / 'test_data'
 args_mock = namedtuple('args_mock', ['date_sources', 'source_name_format'])
 
+
 class TimestampTest(unittest.TestCase):
     def setUp(self):
         self.args = args_mock(['exif'], '%Y%m%d_%H%M%S.jpg')
@@ -36,10 +37,32 @@ class TimestampTest(unittest.TestCase):
                           datadir / 'sammy_sleepy.jpg',
                           self.args)
 
+    def test_fallthrough_ctime(self):
+        self.args.date_sources[0:1] = ['file-name', 'file-created']
+        sleepy = datadir / 'sammy_sleepy.jpg'
+        self.assertEqual(
+            ('file-created', datetime.fromtimestamp(sleepy.stat().st_ctime)),
+            exif_rename.get_timestamp(sleepy, self.args))
+
+    def test_fallthrough_mtime(self):
+        self.args.date_sources[0:1] = ['file-name', 'file-modified']
+        sleepy = datadir / 'sammy_sleepy.jpg'
+        self.assertEqual(
+            ('file-modified', datetime.fromtimestamp(sleepy.stat().st_mtime)),
+            exif_rename.get_timestamp(sleepy, self.args))
+
     def test_no_image(self):
         self.assertRaises(exif_rename.TimestampReadException,
                           exif_rename.get_exif_timestamp,
                           __file__)
+
+    def test_unknown_source(self):
+        self.args.date_sources[0] = 'meow'
+        self.assertRaises(ValueError,
+                          exif_rename.get_timestamp,
+                          datadir / 'sammy_sleepy.jpg',
+                          self.args)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
