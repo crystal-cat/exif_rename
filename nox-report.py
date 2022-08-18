25e03a88eb63be0bff40d3d18d84096fe5ae0133
Fiona Klute <fiona.klute@gmx.de>
2022-08-19 00:57:28 +0200

    Use signature name for Nox session summary
    This way multiple test runs with different Python versions won't have
    the same name any more.

--- nox-report.py
+++ nox-report.py
@@ -6,6 +6,11 @@ with open(sys.argv[1], 'r') as fh:
 
 print('## Nox\n')
 for session in report['sessions']:
+    # looks weird, but is literally how Nox creates a "friendly name"
+    # for a session
+    name = session['signatures'][0] \
+        if session['signatures'] else session['name']
+
     result = session['result']
     if result == 'success':
         mark = 'heavy_check_mark'
@@ -13,4 +18,5 @@ for session in report['sessions']:
         mark = 'large_blue_circle'
     else:
         mark = 'heavy_multiplication_x'
-    print(f'* {session["name"]}: {result} :{mark}:')
+
+    print(f'* {name}: {result} :{mark}:')
